2e451cb2a60a436a4c21e42882d0a928e6b35629
Lewis wanjara <lewicpro@gmail.com>
2021-07-21 16:41:31 +0300

    wowa

--- sparkrecruit/urls.py
+++ sparkrecruit/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import redirect_view
 
 urlpatterns = [
+    path('', redirect_view),
     path('admin/', admin.site.urls),
     path('api/Jobs/', include('Clients.api.urls', namespace='Jobs'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
